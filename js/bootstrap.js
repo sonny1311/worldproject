1eff8db32e16fa30fa8c0aee887dfa7f13c89b41
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-08-13 08:26:40 +0200

    Run universal all-industry playability audit at startup

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -13,11 +13,14 @@ import "./core/CompanyEconomyIntegration.js";
 import "./core/ConnectedEconomyGameplay.js";
 import "./core/CommercialFulfillmentGameplayBridge.js";
 import "./core/CoreRegressionSuite.js";
+import { runAllIndustryPlayabilityAudit } from "./core/AllIndustryPlayabilityAudit.js";
 import { gameAccessGate } from "./core/AccountMultiplayerIntegration.js";
 
 async function startWorldProject(){
     await gameAccessGate.ensureAccess();
     console.log("✅ ACCOUNT FREIGEGEBEN – SPIEL WIRD GELADEN");
+    const industryAudit=runAllIndustryPlayabilityAudit();
+    window.worldProjectIndustryHealth=industryAudit;
     await import("./main.js");
 }
 
