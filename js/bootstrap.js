f9c2aa1e009e395cf112debdb5d284789acdb096
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-08-14 09:12:46 +0200

    Laufende-Aktivitaeten-Uebersicht im Spielerstart aktivieren

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -46,6 +46,7 @@ import "./core/WarehouseSpaceRuntimeGuard.js";
 import "./core/GameActionFeedbackIntegration.js";
 import "./core/GameActionFeedbackBridge.js";
 import "./core/GameSaveStatusIntegration.js";
+import "./core/ActiveOperationsOverviewUI.js";
 import "./core/PremiumExtraPackageUIIntegration.js";
 import "./core/InGameAdminAccessIntegration.js";
 import "./core/CoreRegressionSuite.js";
