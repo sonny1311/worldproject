7f5213317bc2b86f07df219b5c3d2613b0e16508
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-08-14 19:51:36 +0200

    Load customer order product ID compatibility

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -7,6 +7,7 @@ import "./core/DarkMainNavigationIntegration.js";
 import "./core/DarkCommandCenterHeaderIntegration.js";
 import "./core/HomeOperationsDashboardIntegration.js";
 import "./core/HomeWarehouseAndPartialDeliveryIntegration.js";
+import "./core/CustomerOrderProductIdCompatibilityIntegration.js";
 import "./core/HomeDashboardViewportIntegration.js";
 import "./core/TransportVehicleCostIntegration.js";
 import "./core/TransportFuelTimeIntegration.js";
