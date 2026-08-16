c434896710889e708396ebebe7dac6b8ab3815f7
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-08-16 08:26:27 +0200

    Load shared business finance UI

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import "./content/ContentBootstrap.js";
 import "./core/CanonicalIndustryGoodsIntegration.js";
 import "./core/OperationalInventoryBridge.js";
 import "./core/CurrencyPresentationBridge.js";
+import "./core/SharedBusinessFinanceUIIntegration.js";
 import { Company } from "./core/Company.js";
 import { CompanySetup } from "./core/CompanySetup.js";
 
